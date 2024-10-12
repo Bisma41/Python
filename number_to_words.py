f45767cee05814d98da027f6293672facb956071
Bisma41 <152860495+Bisma41@users.noreply.github.com>
2024-10-12 13:21:23 +0500

    Create number_to_words.py

---
+++ number_to_words.py
@@ -0,0 +1,45 @@
+from random import *
+x=randint(0,20)
+if x == 0:
+    print('zero')
+elif x == 1:
+    print('one')
+elif x == 2:
+    print('two')    
+elif x == 3:
+    print('three')
+elif x == 4:
+    print('four')
+elif x == 5:
+    print('five')
+elif x == 6:
+    print('six')
+elif x == 7:
+    print('seven')
+elif x == 8:
+    print('eight')
+elif x == 9:
+    print('nine')
+elif x == 10:
+    print('ten')
+elif x == 11:
+    print('eleven')
+elif x == 12:
+    print('tweleve')
+elif x == 13:
+    print('thirteen')
+elif x == 14:
+    print('fourteen')
+elif x == 15:
+    print('fifteen')
+elif x == 16:
+    print('sixteen')
+elif x == 17:
+    print('seventeen')
+elif x == 18:
+    print('eighteen')
+elif x == 19:
+    print('nineteen')
+elif x == 20:
+    print('twenty')     
+    
